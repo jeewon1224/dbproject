b8b3cff9fe7c8fe81acd87093e52ba55d1a438f1
jeewon94 <jayyy1224@gmail.com>
2021-02-04 17:06:48 +0900

    02/04 랩탑 테스트 : 같이 사용하지 않기!!!

--- Gold/sql/gold_qna.sql
+++ Gold/sql/gold_qna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 21-01-29 02:17
+-- 생성 시간: 21-02-04 03:41
 -- 서버 버전: 10.4.16-MariaDB
 -- PHP 버전: 7.4.12
 
@@ -37,27 +37,6 @@ CREATE TABLE `gold_qna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- 테이블의 덤프 데이터 `gold_qna`
---
-
-INSERT INTO `gold_qna` (`GOLD_QNA_num`, `GOLD_QNA_id`, `GOLD_QNA_tit`, `GOLD_QNA_con`, `GOLD_QNA_reg`, `GOLD_QNA_hit`) VALUES
-(1, 'jwk1224', 'aa', 'vv', '2021-01-25', 0),
-(2, 'jwk1224', 'dd', 'aa', '2021-01-25', 0),
-(3, 'jwk1224', 'ff', 'gg', '2021-01-25', 0),
-(4, 'jwk1224', 'ss', 'dd', '2021-01-25', 0),
-(5, 'jwk1224', 'hh', 'rr', '2021-01-25', 0),
-(6, 'jwk1224', 'ss', 'ff', '2021-01-25', 0),
-(7, 'jwk1224', 'qq', 'aa', '2021-01-25', 0),
-(8, 'jwk1224', 'gj', 'ff', '2021-01-25', 0),
-(9, 'jwk1224', 'gj', 'ff', '2021-01-25', 0),
-(10, 'jwk1224', 'ffff', 'ss', '2021-01-25', 0),
-(11, 'jwk1224', 'ffff', 'ss', '2021-01-25', 2),
-(12, 'jwk1224', 'kkgc', 'cc', '2021-01-25', 3),
-(13, '', '첫 번째 게시글입니다.', '지난해 증권선물위원회가 감사인(회계법인)을 지정한 회사는 1521곳으로 전년보다 297곳(24.3%)이나 증가한 것으로 나타났다.<br />\r\n<br />\r\n26일 금융감독원이 내놓은 ‘2020년 외부감사대상 회사 및 감사인 지정 현황’ 자료를 보면, 지난해 주기적 지정 회사가 462곳, 직권지정이 1059곳이었다. 주기적 지정은 상장사 434곳, 비상장 28곳으로 전년보다 242곳(110%) 증가했다. 직권지정은 상장사 626곳, 비상장사 433곳으로 전년보다 55곳(5.5%) 증가했다.', '2021-01-26', 2),
-(14, 'jwk1224', 'Hi', 'Hello!', '2021-28-01', 55),
-(15, 'jwk1224', 'Ready', 'set', '2021-28-01', 107);
-
---
 -- 덤프된 테이블의 인덱스
 --
 
@@ -75,7 +54,7 @@ ALTER TABLE `gold_qna`
 -- 테이블의 AUTO_INCREMENT `gold_qna`
 --
 ALTER TABLE `gold_qna`
-  MODIFY `GOLD_QNA_num` int(11) NOT NULL AUTO_INCREMENT COMMENT '고유번호', AUTO_INCREMENT=16;
+  MODIFY `GOLD_QNA_num` int(11) NOT NULL AUTO_INCREMENT COMMENT '고유번호', AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
